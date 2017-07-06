606c88ae209c9ee27588745ed2850d048ff1de41
Dixi KIYINDOU <dixilindez@gmail.com>
2017-07-06 02:35:58 +0200

    first commit

---
+++ src/main/java/com/scholanova/groupe2/libraryproject/webservices/App.java
@@ -0,0 +1,13 @@
+package com.scholanova.groupe2.libraryproject.webservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
